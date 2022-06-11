fea39e2ae90463bcbfe4b43bf156b7734825a4e7
zeinab-Ahmed <44482247+zozoahmed@users.noreply.github.com>
2022-06-11 21:39:32 +0200

    Update taskDay5.php

--- day5/taskDay5.php
+++ day5/taskDay5.php
@@ -76,6 +76,35 @@ else
 
      
 }
+    if (!empty($_FILES['image']['name'])) {
+
+        $tempName  = $_FILES['image']['tmp_name'];
+        $imageName = $_FILES['image']['name'];
+        $imageType = $_FILES['image']['type'];
+
+        $extensionArray = explode('/', $imageType);
+        $extension =  strtolower( end($extensionArray));
+
+        $allowedExtensions = ['png', 'jpg', 'jpeg', 'webp'];    // PNG 
+
+        if (in_array($extension, $allowedExtensions)) {
+
+            $finalName = uniqid() . time() . '.' . $extension;
+
+            $disPath = 'uploads/' . $finalName;
+
+            if (move_uploaded_file($tempName, $disPath)) {
+                echo 'File Uploaded Successfully';
+            } else {
+                echo 'File Uploaded Failed';
+            }
+        } else {
+            echo 'File Type Not Allowed';
+        }
+    } else {
+        echo 'Please Select File';
+    }
+
 
 
 
